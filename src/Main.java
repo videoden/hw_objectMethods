147bd0537881be34575fa919e40dbd0ebf7c0341
qmar <video4den@gmail.com>
2023-01-15 12:01:40 +0500

    создал ветку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author victorPelevin = new Author("Виктор", "Пелевин");
+        Book transhumanism = new Book("Трансгуманизм", victorPelevin, 2021);
+        System.out.println(transhumanism.getTitle() + " " + transhumanism.getYear() + " " + transhumanism.getAuthor().getName() + " " + transhumanism.getAuthor().getSurname());
+
+        Author ivanEfremov = new Author("Иван", "Ефремов");
+        Book hourOfTheBull = new Book("Час быка", ivanEfremov ,1970);
+
+        hourOfTheBull.setYear(1971);
+        System.out.println(hourOfTheBull.getTitle() + " " + hourOfTheBull.getYear() + " " + hourOfTheBull.getAuthor().getName() + " " + hourOfTheBull.getAuthor().getSurname());
+
     }
 }
